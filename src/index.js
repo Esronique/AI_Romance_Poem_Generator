a971b54ee13336224d6db038b50d55fde62fe88d
Esronique Du Plessis <37573027+Esronique@users.noreply.github.com>
2025-08-13 19:47:58 -0700

    Improve User Experience

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 function displayPoem(reponse){
     console.log("Poem Generated");
-   new Typewriter("#poem-body", {
+   new Typewriter("#poem", {
     strings: reponse.data.answer,
     autoStart: true,
     delay: 2,
@@ -21,11 +21,19 @@ function generatePoem(event){
     let context = "You are a romantic poem expert that writes unique poems. The poem should be maximum 10 lines without a heading using basic HTML. Sign the poem at the bottom in bold Italic '-SheCodes AI' ";
     let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-    axios.get(apiURL).then(displayPoem);
+    
 
-    console.log("Generating Poem");
+
+     let poemElement = document.querySelector("#poem");
+     poemElement.classList.remove("hidden");
+     poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${userInstructionInput.value}</div>`;
+
+
+     console.log("Generating Poem");
     console.log(`Promt: ${prompt}`);
     console.log(`Context: ${context}`);
+
+    axios.get(apiURL).then(displayPoem);
 
 
 
